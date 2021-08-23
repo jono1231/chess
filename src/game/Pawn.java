64c1af1bacca99f44a54bb4160e8965d10446a82
jono1231 <jonwang2015@gmail.com>
2021-08-23 11:41:48 -0700

    Adding more things, board, piece, and square

--- src/game/Pawn.java
+++ src/game/Pawn.java
@@ -1,10 +1,41 @@
 package game;
 
+import java.util.List;
 
 public class Pawn implements Piece{
+	private int xPos;
+	private int yPos;
+	private boolean color;
+	
+	//Constructor method
+	public Pawn(int xStart, int yStart, boolean pColor) {
+		xPos = xStart;
+		yPos = yStart;
+		color = pColor;
+	}
 
-	public Pawn(int xStart, int yStart, Boolean color) {
+	//Move method - returns true if attempted move is possible
+	//returns false if attempted move isn't possible
+	public boolean move(int x1, int y1) {
 		
+		return false;
+	}
+
+	//Hurr Durr
+	public boolean getColor() {
+		return color;
+	}
+	
+	//Draws piece on the board, I rely on hoggy for this one
+	public void draw() {
+		
+	}
+	
+	//Gets the piece on the squares that a pawn can move to
+	//In this case, a pawn needs to get pieces in front and on the left or right
+	public List<Square> getLegalMoves(Board b) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/game/Piece.java
+++ src/game/Piece.java
@@ -1,9 +1,13 @@
 package game;
 
+import java.util.List;
 
 public interface Piece{
+	boolean move(int x1, int y1);
+	boolean getColor();
+	void draw();
 	
-	private boolean isLegal(int xFinal, int yFinal) {
-		return true;
-	}
+	//In theory this returns for all places a piece can move
+	//Null if the square is empty, and a piece if it isn't
+	List<Square> getLegalMoves(Board b);
 }
