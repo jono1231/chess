a1e217181424f330a59cfc3c2d9d6da991146f9a
jono1231 <jonwang2015@gmail.com>
2021-08-23 21:28:59 -0700

    added king class

---
+++ src/game/King.java
@@ -0,0 +1,37 @@
+package game;
+
+import java.util.List;
+
+public class King implements Piece {
+
+	@Override
+	public boolean move(int x1, int y1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean getColor() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void isCaptured() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Square> getLegalMoves(Board b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
